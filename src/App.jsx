ee003fb0b699527de3e66e9bad2ece12e7aeb92c
Bruna Fonseca <brunafonseca@gmail.com>
2026-09-10 16:44:18 -0300

    feat: implement dice display and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,22 @@
+import Die from './components/Die'
+
 export default function App() {
-	return <main></main>
+	return (
+		<main>
+			<main>
+				<div className="dice-container">
+					<Die value="5" />
+					<Die value="1" />
+					<Die value="6" />
+					<Die value="3" />
+					<Die value="5" />
+					<Die value="5" />
+					<Die value="2" />
+					<Die value="5" />
+					<Die value="5" />
+					<Die value="3" />
+				</div>
+			</main>
+		</main>
+	)
 }
---
+++ src/components/Die.jsx
@@ -0,0 +1,3 @@
+export default function Die(props) {
+	return <button className="button">{props.value}</button>
+}
