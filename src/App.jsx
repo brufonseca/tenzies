f80579871ab2f58eeed6e136008ccf1eda907d1e
Bruna Fonseca <brunafonseca@gmail.com>
2026-09-11 10:46:13 -0300

    feat: implement dynamic dice generation and display

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,30 @@
+import { useState } from 'react'
 import Die from './components/Die'
 
 export default function App() {
+	const [randomNumbers, setRandomNumbers] = useState(generateAllNewDice())
+
+	function getRandomInt(min, max) {
+		min = Math.ceil(min)
+		max = Math.floor(max)
+		return Math.floor(Math.random() * (max - min + 1)) + min
+	}
+
+	function generateAllNewDice() {
+		const randomNumbers = []
+		for (let i = 1; i <= 10; i++) {
+			randomNumbers.push(getRandomInt(1, 6))
+		}
+
+		return randomNumbers
+	}
+
+	const dieComponents = randomNumbers.map((num) => <Die value={num} />)
+
 	return (
 		<main>
 			<main>
-				<div className="dice-container">
-					<Die value={5} />
-					<Die value={1} />
-					<Die value={6} />
-					<Die value={3} />
-					<Die value={5} />
-					<Die value={5} />
-					<Die value={5} />
-					<Die value={2} />
-					<Die value={5} />
-					<Die value={5} />
-					<Die value={3} />
-				</div>
+				<div className="dice-container">{dieComponents}</div>
 			</main>
 		</main>
 	)
